e534be20db186fd5c8c08a97885fce8e1ad886d7
Ken-Alviola <kenalviola@gmail.com>
2021-06-28 15:02:08 -0500

    higher than 75 marks

--- Basic_Select.sql
+++ Basic_Select.sql
@@ -65,3 +65,18 @@ where city not REGEXP '[aeiou]$';
 Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
 Your result cannot contain duplicates.*/
 
+select distinct city
+from station
+where city not REGEXP '^[aeiou].*[aeiou]$';
+
+/*Weather Observation Station 12
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+Your result cannot contain duplicates. */
+
+select distinct city
+from station
+where city not REGEXP '^[aeiou]' and city not REGEXP '[aeiou]$';
+
+/*Higher Than 75 Marks
+Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending 
+in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID. */
