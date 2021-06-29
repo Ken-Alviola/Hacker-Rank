1d88f9ab32ad1a3d6e6d02c8cb3f9e983800db6a
Ken-Alviola <kenalviola@gmail.com>
2021-06-29 17:34:06 -0500

    emp salaries

--- Basic_Select.sql
+++ Basic_Select.sql
@@ -80,3 +80,20 @@ where city not REGEXP '^[aeiou]' and city not REGEXP '[aeiou]$';
 /*Higher Than 75 Marks
 Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending 
 in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID. */
+select name
+from students
+where marks > 75
+order by RIGHT(NAME, 3), ID ASC;
+
+/*Employee Names
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.*/
+
+select name
+from employee
+order by name;
+
+/*Employee Salaries
+Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having 
+a salary greater than 2k per month who have been employees for less than 10  months.
+ Sort your result by ascending employee_id.
+
