dd592e3e360c00326c3ab55f57a9bfe9a01eb189
Ken-Alviola <kenalviola@gmail.com>
2021-07-02 19:22:03 -0500

    Japan population

--- basic_aggregations.sql
+++ basic_aggregations.sql
@@ -7,3 +7,22 @@ where population > 100000;
 
 /* Revising Aggregations - The Sum Function
 Query the total population of all cities in CITY where District is California.*/
+
+select sum(population)
+from city
+where district = 'california';
+
+/* Revising Aggregations - Averages
+Query the average population of all cities in CITY where District is California.*/
+select avg(population)
+from city
+where district = 'california';
+
+/* Average Population
+Query the average population for all cities in CITY, rounded down to the nearest integer. */
+
+select round(avg(population))
+from city;
+
+/*Japan Population
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN. */
