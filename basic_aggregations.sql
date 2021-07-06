e4edd3a599cce2892ffac190677d1855935beed5
Ken-Alviola <kenalviola@gmail.com>
2021-07-06 14:00:00 -0500

    the blunder

--- basic_aggregations.sql
+++ basic_aggregations.sql
@@ -26,3 +26,18 @@ from city;
 
 /*Japan Population
 Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN. */
+select sum(population)
+from city
+where countrycode = 'JPN';
+
+/* Population Density Difference
+Query the difference between the maximum and minimum populations in CITY.*/
+
+select max(population) - min(population)
+from city;
+
+/* The Blunder
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's  key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
+Input Format*/
+
