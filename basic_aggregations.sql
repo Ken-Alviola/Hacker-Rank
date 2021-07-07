7a05caba850f9905c60ed4d356b5d8003f784dff
Ken-Alviola <kenalviola@gmail.com>
2021-07-07 11:32:58 -0500

    top earners

--- basic_aggregations.sql
+++ basic_aggregations.sql
@@ -41,3 +41,9 @@ Samantha was tasked with calculating the average monthly salaries for all employ
 Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
 Input Format*/
 
+SELECT CEIL((AVG(salary)) - (AVG(REPLACE(salary, 0, '')))) AS avg_salary FROM employees;
+
+/*Top Earners
+We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as 
+the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.*/
+
