d6b4d71fc2902bde074c3da4d570ad4300c6a714
Ken-Alviola <kenalviola@gmail.com>
2021-07-08 14:16:31 -0500

    weather obs 2

--- basic_aggregations.sql
+++ basic_aggregations.sql
@@ -47,3 +47,13 @@ SELECT CEIL((AVG(salary)) - (AVG(REPLACE(salary, 0, '')))) AS avg_salary FROM em
 We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as 
 the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.*/
 
+select (salary * months) as earnings, count(*)
+from employee
+group by 1
+order by earnings desc limit 1;
+
+/*Weather Observation Station 2
+Query the following two values from the STATION table:
+The sum of all values in LAT_N rounded to a scale of  2 decimal places.
+The sum of all values in LONG_W rounded to a scale of  2 decimal places.*/
+
