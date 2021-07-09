60812af38d0a065f2d0d916dfabc398d03e53744
Ken-Alviola <kenalviola@gmail.com>
2021-07-09 15:32:01 -0500

    weather 13

--- basic_aggregations.sql
+++ basic_aggregations.sql
@@ -57,3 +57,9 @@ Query the following two values from the STATION table:
 The sum of all values in LAT_N rounded to a scale of  2 decimal places.
 The sum of all values in LONG_W rounded to a scale of  2 decimal places.*/
 
+select round(sum(lat_n),2), round(sum(long_w),2)
+from station;
+
+/*Weather Observation Station 13
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.788 and less than 137.2345 . 
+Truncate your answer to 4 decimal places.
