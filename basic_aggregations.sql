2961ee6054da0bbdb5eb2069b6f91ec614e2cdc9
Ken-Alviola <kenalviola@gmail.com>
2021-07-11 15:20:58 -0500

    weather 15

--- basic_aggregations.sql
+++ basic_aggregations.sql
@@ -71,3 +71,10 @@ where lat_n > 38.788 and lat_n < 137.2345;
 /* Weather Observation Station 14
 Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. 
 Truncate your answer to 4 decimal places. */
+select round(max(LAT_N),4)
+from station
+where LAT_N < 137.2345;
+
+/*Weather Observation Station 15
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. 
+Round your answer to 4  decimal places. */
