7e63aa5b5250f17670e45ee8fa8bfd4dbd7fe5c6
Ken-Alviola <kenalviola@gmail.com>
2021-07-14 10:24:05 -0500

    weather 17

--- basic_aggregations.sql
+++ basic_aggregations.sql
@@ -88,4 +88,12 @@ limit 1;
 /* Weather Observation Station 16
 Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780 . 
 Round your answer to 4 decimal places. */
+select round(min(lat_n),4)
+from station
+where lat_n > 38.7780;
 
+
+/*Weather Observation Station 17
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780.
+ Round your answer to  4 decimal places. */
+ 
