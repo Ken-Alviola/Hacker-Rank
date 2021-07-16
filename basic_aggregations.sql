83eeadea6b31703b672acfd801faefddbbb119cb
Ken-Alviola <kenalviola@gmail.com>
2021-07-16 16:29:47 -0500

    arithmetic python

--- basic_aggregations.sql
+++ basic_aggregations.sql
@@ -96,4 +96,21 @@ where lat_n > 38.7780;
 /*Weather Observation Station 17
 Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780.
  Round your answer to  4 decimal places. */
- 
+
+select round(long_w,4)
+from station
+where lat_n > 38.778
+order by lat_n asc
+limit 1;
+
+/*Weather Observation Station 18
+Consider p1(a,b) and p2(c,d)  to be two points on a 2D plane.
+ a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points p1 and p2 and round it to a scale of  4decimal places.
+
+The distance between two points measured along axes at right angles. 
+In a plane with p1 at (x1, y1) and p2 at (x2, y2), it is |x1 - x2| + |y1 - y2|. 
+|a - c| + |b -d| */
