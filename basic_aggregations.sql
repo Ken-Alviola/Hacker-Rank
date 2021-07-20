702ed6c3e9f399028476c7149697ae6a16624cdc
Ken-Alviola <kenalviola@gmail.com>
2021-07-20 12:35:34 -0500

    joins

--- basic_aggregations.sql
+++ basic_aggregations.sql
@@ -132,4 +132,14 @@ from station;
  median is defined as a number separating the higher half of a data set from the lower half. 
  Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  4 decimal places. */
 
- 
+ set @rowindex := -1; /* 1) creates an index*/ 
+/* 3) the outer query will select the average of the 2(for odd no. of values)/1(for even) values we found in the middle of the sorted array */
+select round(avg(lat_n),4)
+from
+/* 2) the index will increment for each new value of lat_n it finds, and sort them by lat_n
+*/
+(select @rowindex:=@rowindex+1 as rowindex, lat_n 
+from station
+order by lat_n) as l 
+where l.rowindex in (floor(@rowindex/2), ceil(@rowindex/2));
+
