9882356ba8186f93b41a8e4d76e75d40ed9c1b5b
Ken-Alviola <kenalviola@gmail.com>
2021-07-21 12:40:16 -0500

    african cities and runner up score

--- basic_join.sql
+++ basic_join.sql
@@ -1,3 +1,17 @@
+/*Population Census
+Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'. */
+select sum(city.population)
+from city join country on city.countrycode = country.code
+where country.continent = 'Asia';
+
+
+/*African Cities
+Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns. */
+
+
+
+
 /*Average Population of Each Continent
 Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
 Note: CITY.CountryCode and COUNTRY.Code are matching key columns. */
