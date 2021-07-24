fe2dc72abfdf938a0dd33ec061369a977d3e30b7
Ken-Alviola <kenalviola@gmail.com>
2021-07-24 15:36:04 -0500

    type of triangle

--- basic_join.sql
+++ basic_join.sql
@@ -22,6 +22,25 @@ select country.continent, round(avg(city.population)-0.5)
 from city join country on city.countrycode = country.code
 group by continent;
 
+/*Type of Triangle
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+Equilateral: It's a triangle with  sides of equal length.
+Isosceles: It's a triangle with  sides of equal length.
+Scalene: It's a triangle with  sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.*/
+
+SELECT CASE             
+            WHEN A + B > C AND B + C > A AND A + C > B THEN
+                CASE 
+                    WHEN A = B AND B = C THEN 'Equilateral'
+                    WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+                    ELSE 'Scalene'
+                END
+            ELSE 'Not A Triangle'
+        END
+FROM TRIANGLES;
+
+
 /*The Report
 You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
 
